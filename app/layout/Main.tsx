ca0dd7eafc0cd1a9158616545d70df4fea6e58d7
Bijay Shrestha <bijayashrestha929@gmail.com>
2024-04-18 00:33:40 +0545

    Refactor HeroSection  and add Discover component

--- app/layout/Main.tsx
+++ app/layout/Main.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/Components/HeroSection"
 import Navbar from "@/Components/Navbar"
+import Discover from "./Discover/Discover"
 const Main = () => {
     return (
-        <div className="px-16">
+        <div className="px-16 py-4">
             <Navbar />
             <HeroSection />
+            <Discover />
         </div>
     )
 }
