3af8e0e1ee12b83dcc0df2c9bab614c79d11c58d
Bijay Shrestha <bijayashrestha929@gmail.com>
2024-04-20 17:18:08 +0545

    updated navbar

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
 
     return (
         <nav className="fixed top-0 left-0 w-full bg-black z-10 ">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:mx-40">
                 <div className="flex justify-between h-24">
                     <div className="flex-shrink-0 flex items-center cursor-pointer">
                         <a href="/">
